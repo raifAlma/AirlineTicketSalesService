3631dc44234efbd28f2a5680c12e0e17187e46f0
Raif Almakaev <raif.almakaev@gmail.com>
2026-08-06 19:28:42 +0300

    add generate secrets example, update config

---
+++ src/infrastructure/authentication/user_manager.py
@@ -0,0 +1,30 @@
+import logging
+import uuid
+
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, UUIDIDMixin
+
+import settings
+from infrastructure.database.postgresql.models import User
+
+
+log = logging.getlogger(__name__)
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = settings.AccessToken.resert_passwerd_token_secret
+    verification_token_secret = settings.AccessToken.verification_token_secret
+
+    async def on_after_register(self, user: User, request: Request | None = None):
+        log.warning(f"User %r has registered.", user.id)
+
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Request | None = None
+    ):
+        log.warning(f"User %r has forgot their password. Reset token: %r", user.id, token)
+
+    async def on_after_request_verify(
+        self, user: User, token: str, request: Request | None = None
+    ):
+        log.warning(f"Verification requested for user %r. Verification token: %r", user.id, token)
+
+
