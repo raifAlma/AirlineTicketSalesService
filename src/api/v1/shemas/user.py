6b353a23c9497b0a170b67054747b520139dfe7b
Raif Almakaev <raif.almakaev@gmail.com>
2026-08-10 15:36:17 +0300

    create user shemas

---
+++ src/api/v1/shemas/user.py
@@ -0,0 +1,20 @@
+import uuid
+from typing import Optional
+
+from fastapi_users import schemas
+
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    full_name: str
+    phone: str
+    role: str
+
+class UserCreate(schemas.BaseUserCreate):
+    full_name: str
+    phone: str
+    role: str = "user"  # значение по умолчанию
+
+class UserUpdate(schemas.BaseUserUpdate):
+    full_name: Optional[str] = None
+    phone: Optional[str] = None
+    role: Optional[str] = None
