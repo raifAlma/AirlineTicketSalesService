464b48ee366286e0086635c267f6eb6e72849038
Raif Almakaev <raif.almakaev@gmail.com>
2026-08-10 16:03:53 +0300

    move fastapi users routers helper to routers package

--- src/api/api_v1/auth.py
+++ src/api/api_v1/auth.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 
+from api.api_v1.fastapi_users import fastapi_users
+from api.dependencies.authentication.backend import authentication_backend
+
 router = APIRouter(
     prefix="/api/api_v1/auth",
     tags=["auth"],
+)
+
+router.include_router(
+    router = fastapi_users.get_auth_router(authentication_backend),
 )
--- src/api/api_v1/fastapi_users.py
+++ src/api/api_v1/fastapi_users.py
@@ -1,8 +1,7 @@
 import uuid
-
-from api.api_v1.dependencies.user_manager import get_user_manager
+from api.dependencies.authentication.backend import authentication_backend
+from api.dependencies.authentication.user_manager import get_user_manager
 from fastapi_users import FastAPIUsers
-from .backend import authentication_backend
 from infrastructure.database.postgresql.models import User
 fastapi_users = FastAPIUsers[User, uuid.UUID](
     get_user_manager,
