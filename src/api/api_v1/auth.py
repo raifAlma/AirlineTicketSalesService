b37083c37ef3ef220093007256f4bb6d8ea38b42
Raif Almakaev <raif.almakaev@gmail.com>
2026-08-13 19:24:11 +0300

    user manager handle reset password

--- src/api/api_v1/auth.py
+++ src/api/api_v1/auth.py
@@ -18,3 +18,14 @@ router.include_router(
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
 )
+
+router.include_router(
+    fastapi_users.get_verify_router(UserRead),
+)
+
+#/forgot-password
+#/reset-password
+
+router.include_router(
+    fastapi_users.get_reset_password_router(),
+)
