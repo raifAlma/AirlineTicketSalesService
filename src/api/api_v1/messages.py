4027ee63845249d803ba3a626ed27f45f1919d9e
Raif Almakaev <raif.almakaev@gmail.com>
2026-08-15 00:21:12 +0300

    create views for authenticates users and superusers

---
+++ src/api/api_v1/messages.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from fastapi import Depends, HTTPException
+
+from api.api_v1.fastapi_users import current_active_user, current_active_superuser
+from api.shemas.user import UserRead
+from infrastructure.database.postgresql.models import User
+
+router = APIRouter(
+    prefix="/messeges",
+    tags=["Messeges"],
+)
+
+@router.get('')
+def get_user_messages(
+        user: User = Depends(current_active_user),
+):
+    return {"message": "Hello User",
+            'user': UserRead.model_validate(user)}
+
+
+@router.get('/secrets')
+def get_superuser_messages(
+        user: User = Depends(current_active_superuser),
+):
+    return {"message": "Hello SuperUser",
+            'user': UserRead.model_validate(user)}
