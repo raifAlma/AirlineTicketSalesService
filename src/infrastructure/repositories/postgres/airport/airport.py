aacb24efb03b9525d0800c57c60296a79ec55523
Raif Almakaev <raif.almakaev@gmail.com>
2026-08-23 18:50:00 +0300

    added route for search airport

--- src/infrastructure/repositories/postgres/airport/airport.py
+++ src/infrastructure/repositories/postgres/airport/airport.py
@@ -1,13 +1,12 @@
-from uuid import UUID
-
+from typing import List
 from fastapi import HTTPException
-from sqlalchemy import select
+from sqlalchemy import select,  or_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.schemas.airport import CreateAirportSchema
 from domain.abstract_repositories.airport import AbstractAirportRepository
 from infrastructure.database.postgresql.models import Airport
-from infrastructure.repositories.postgres.airport.exception import AirportAlreadyExists
+from infrastructure.repositories.postgres.airport.exception import AirportAlreadyExists, AirportNotFound
 from infrastructure.types import AirportIdType
 
 
@@ -20,7 +19,7 @@ class PostgreSQLAirportRepository(AbstractAirportRepository):
         res = await self.session.execute(smt)
         existing_airport = res.scalar_one_or_none()
         if existing_airport:
-            raise AirportAlreadyExists()
+            raise AirportAlreadyExists(code=payload.code)
 
         airport = Airport(
             code=payload.code,
@@ -37,5 +36,18 @@ class PostgreSQLAirportRepository(AbstractAirportRepository):
         res = await self.session.execute(smt)
         airport = res.scalar_one_or_none()
         if airport is None:
-            raise HTTPException(status_code=404, detail="Airport not found")
+            raise AirportNotFound(airport_id=id)
         return airport
+
+    async def search (self, query: str) -> List[Airport]:
+        pattern = f"%{query}%"
+        stmt = select(Airport).where(
+        or_(
+            Airport.name.ilike(pattern),
+            Airport.city.ilike(pattern),
+            Airport.code == query.upper(),
+        )
+    )
+        result = await self.session.execute(stmt)
+        airport = result.scalars().all()
+        return List(airport)
