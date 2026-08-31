937a8178f7fc6a0527d01be92f8b6dfdd6628c1c
Raif Almakaev <raif.almakaev@gmail.com>
2026-08-31 15:26:46 +0300

    added test for usecase

--- tests/domain/test_airport_domain.py
+++ tests/domain/test_airport_domain.py
@@ -1,18 +1,17 @@
-import uuid
 from unittest.mock import AsyncMock
 
 import pytest
 from httpx import ASGITransport, AsyncClient
 
-from app import app
-from infrastructure.database.postgresql.models import Airport
-from infrastructure.repositories.postgres.airport.exception import AirportNotFound
+from api.schemas.airport import UpdateAirportSchema
 from src.domain.entities.airport import (
     AirportCreateData,
+    AirportUpdateData,
     InvalidAirportCode,
     InvalidAirportField,
 )
 from usecases.airport.get.implementation import PostgreSQLGetAirportUseCase
+from usecases.airport.update.implementation import PostgreSQLUpdateAirportUseCase
 
 
 def test_create_airport_with_valid_data_succeeds():
@@ -56,10 +55,29 @@ def test_create_airport_with_empty_field_raises_error(field_name, kwargs):
         AirportCreateData(**kwargs)
 
 
-def create_airport_with_city_101_symbol_raises_error():
-    with pytest.raises(InvalidAirportCode):
-        AirportCreateData(
-            code="AAA", name="Heathrow", city="London" * 101, country="UK"
-        )
+def test_create_airport_with_city_101_symbols_raises_error():
+    with pytest.raises(InvalidAirportField):
+        AirportCreateData(code="AAA", name="Heathrow", city="A" * 101, country="UK")
 
 
+async def test_update_airport_usecase_calls_repository_correctly():
+    # Arrange
+    mock_repo = AsyncMock()
+    mock_repo.update.return_value = "Airport(fake result)"
+
+    mock_uow = AsyncMock()
+    mock_uow.__aenter__.return_value = mock_uow
+    mock_uow.repository = mock_repo
+
+    usecase = PostgreSQLUpdateAirportUseCase(uow=mock_uow)
+    payload = UpdateAirportSchema(name="New Heathrow")
+    airport_id = "airport-id-123"
+
+    # Act
+    result = await usecase.execute(airport_id, payload)
+
+    # Assert
+    expected_data = AirportUpdateData(
+        code=None, name="New Heathrow", city=None, country=None
+    )
+    mock_repo.update.assert_called_once_with(airport_id, expected_data)
