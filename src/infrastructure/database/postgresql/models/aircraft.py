e0df1c3aa18f92c3c04f21caa22eeb32db176b2a
Raif Almakaev <raif.almakaev@gmail.com>
2026-09-01 15:43:10 +0300

    added rows business/per

--- src/infrastructure/database/postgresql/models/aircraft.py
+++ src/infrastructure/database/postgresql/models/aircraft.py
@@ -1,7 +1,6 @@
 import uuid
 
 from sqlalchemy import Integer, String
-from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..base import Base
@@ -10,10 +9,10 @@ from .flight import Flight
 
 class Aircraft(Base):
     __tablename__ = "aircraft"
-    id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
-    )
+    id: Mapped[uuid.UUID] = mapped_column(..., primary_key=True, default=uuid.uuid4)
     model: Mapped[str] = mapped_column(String(100), nullable=False)
-    capacity: Mapped[int] = mapped_column(Integer, nullable=False)
-
+    rows: Mapped[int] = mapped_column(Integer, nullable=False)
+    seats_per_row: Mapped[int] = mapped_column(Integer, nullable=False, default=6)
+    business_rows: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    # capacity можно вычислять: rows * seats_per_row
     flights: Mapped[list[Flight]] = relationship(back_populates="aircraft")
