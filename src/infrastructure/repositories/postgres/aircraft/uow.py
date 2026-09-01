23805bbb82aa6fea4b8606d71a9afddde03cfdff
Raif Almakaev <raif.almakaev@gmail.com>
2026-09-01 16:01:18 +0300

    create uow for aircraft

---
+++ src/infrastructure/repositories/postgres/aircraft/uow.py
@@ -0,0 +1,30 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from infrastructure.repositories.postgres.aircraft.aircraft import PostgreSQLAircraftRepository
+from infrastructure.repositories.postgres.airport import PostgreSQLAirportRepository
+
+
+class PostgreSQLAircraftUnitOfWork:
+    def __init__(self, session: AsyncSession):
+        self._session: AsyncSession = session
+
+        self.repository: PostgreSQLAircraftRepository | None = None
+
+    async def __aenter__(self):
+        self.repository = PostgreSQLAircraftRepository(self._session)
+        return self
+
+    async def __aexit__(self, exc_type: Exception | None, exc_val, traceback):
+        if exc_type is not None:
+            await self.rollback()
+        else:
+            await self.commit()
+
+        await self._session.close()
+        self.repository = None
+
+    async def commit(self):
+        await self._session.commit()
+
+    async def rollback(self):
+        await self._session.rollback()
