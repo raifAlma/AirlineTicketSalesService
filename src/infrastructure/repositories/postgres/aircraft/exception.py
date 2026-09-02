1ef6f0c33871e714137dc6179fb8752d1400c580
Raif Almakaev <raif.almakaev@gmail.com>
2026-09-02 11:19:31 +0300

    added exception AircraftNotFound

--- src/infrastructure/repositories/postgres/aircraft/exception.py
+++ src/infrastructure/repositories/postgres/aircraft/exception.py
@@ -1,3 +1,6 @@
+from infrastructure.types import AircraftIdType
+
+
 class AircraftError(Exception):
     """Базовое исключение для всех ошибок, связанных с Airport."""
 
@@ -11,5 +14,13 @@ class AircraftAlreadyExists(AircraftError):
             f"Aircraft with model {model!r} already exists"
             if model
             else "Aircraft already exists, check the entered data"
+        )
+        super().__init__(message)
+
+class AircraftNotFound(AircraftError):
+    def __init__(self, id: AircraftIdType | None = None):
+        self.id = id
+        message = (
+            f"Aircraft with id {id!r} does not exist"
         )
         super().__init__(message)
