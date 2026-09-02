9c5231fe432af80154a9a10777721708eade1867
Raif Almakaev <raif.almakaev@gmail.com>
2026-09-02 11:18:32 +0300

    added method for get_by_id

--- src/domain/abstract_repositories/aircraft.py
+++ src/domain/abstract_repositories/aircraft.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
 
 from domain.entities.aircraft import AircraftCreateData
+from infrastructure.database.postgresql.models import Aircraft
+from infrastructure.types import AircraftIdType
 
 
 class AbstractAircraftRepository(ABC):
 
     @abstractmethod
-    async def create(self, payload: AircraftCreateData):
+    async def create(self, payload: AircraftCreateData) -> Aircraft:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_id(self, id: AircraftIdType) -> Aircraft:
         raise NotImplementedError
