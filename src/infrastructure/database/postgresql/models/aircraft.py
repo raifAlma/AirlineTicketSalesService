5810131dd0a1b08dde5810dcc41556f3d6a8c7b3
Raif Almakaev <raif.almakaev@gmail.com>
2026-09-02 11:18:57 +0300

    deleted "..." from id

--- src/infrastructure/database/postgresql/models/aircraft.py
+++ src/infrastructure/database/postgresql/models/aircraft.py
@@ -9,7 +9,7 @@ from .flight import Flight
 
 class Aircraft(Base):
     __tablename__ = "aircraft"
-    id: Mapped[uuid.UUID] = mapped_column(..., primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     model: Mapped[str] = mapped_column(String(100), nullable=False)
     rows: Mapped[int] = mapped_column(Integer, nullable=False)
     seats_per_row: Mapped[int] = mapped_column(Integer, nullable=False, default=6)
